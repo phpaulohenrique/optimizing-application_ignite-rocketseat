66af03382b2bc0af742a129987835b0a2217ebfd
Paulo Henrique <paulo.henrique1080p@gmail.com>
2022-11-20 15:53:13 -0300

    first commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+
+interface HeaderProps {
+    selectedGenreTitle: string
+}
+
+export function Header({ selectedGenreTitle }: HeaderProps) {
+    return (
+        <header>
+            <span className="category">
+                Categoria:<span> {selectedGenreTitle}</span>
+            </span>
+        </header>
+    );
+}
